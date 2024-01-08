1592754c9508307578679f6a39218339f1a9d595
Олег Ф <olegfedoruk777@gmail.com>
2024-01-08 19:23:07 +0200

    Initial commit

---
+++ src/homeWarkQA.java
@@ -0,0 +1,8 @@
+public class homeWarkQA {
+    public static void main(String[]args){
+        int a = 7;
+        int b = 9;
+        System.out.println(a+b);
+        System.out.println(a-b);
+    }
+}
